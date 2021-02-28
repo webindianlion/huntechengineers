cbaf2a5aa83ac46e7d37347900caed32594b89e4
Vinod Kumar <webindianlion@gmail.com>
2021-02-28 15:41:49 +0530

    semi final changes

--- common/footer.php
+++ common/footer.php
@@ -38,9 +38,7 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
   <script src="https://use.fontawesome.com/20483d011f.js"></script>
-  
-  <script src="./js/typewriter/typewriter.js"></script>
-  <script src="./js/typewriter/typed.min.js"></script>
+
   <script src="./js/aos.js"></script>
   <script>
     AOS.init();
--- common/head.php
+++ common/head.php
@@ -6,28 +6,8 @@
         <meta name="Keywords" content="Huntechengineers">
         <meta name="Description" content="Huntechengineers">
 
-        <!-- favicon STARTS  -->
-        <link rel="apple-touch-icon" sizes="57x57" href="./images/favicon/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="./images/favicon/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="./images/favicon/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="./images/favicon/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="./images/favicon/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="./images/favicon/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="./images/favicon/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="./images/favicon/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192"  href="./images/favicon/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="./images/favicon/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
-        <link rel="manifest" href="/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
-
-        <!-- favicon ENDS  -->
-
-        <!-- <link rel="stylesheet" href="https://owlcarousel2.github.io/OwlCarousel2/assets/css/docs.theme.min.css"> -->
+        <link rel="shortcut icon" href="./images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
 
         <!-- Owl Stylesheets -->
         <link rel="stylesheet" href="https://owlcarousel2.github.io/OwlCarousel2/assets/owlcarousel/assets/owl.carousel.min.css">
@@ -42,8 +22,7 @@
         <![endif]-->
 
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">        
-        <link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css" >        
-        <!-- <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
+        <link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css" >                
         <link rel="stylesheet" href="./css/aos.css" />         
         <link rel="stylesheet" href="./css/bootstrap-icons.css" />         
         <link rel="stylesheet" href="./css/style.css">           
